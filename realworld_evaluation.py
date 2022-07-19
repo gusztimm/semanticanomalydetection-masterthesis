400badd7b54c358b6b28bccd336d62e7a5aaf80c
gusztimm <herrmegyesi@gmail.com>
2022-07-19 11:47:27 +0200

    List of configs finalized

--- realworld_evaluation.py
+++ realworld_evaluation.py
@@ -30,7 +30,11 @@ event_key_map = {
 def run_evaluation():
     configs = [
         Configuration(sim_mode=SimMode.SEMANTIC_SIM, sim_threshold=0.7, kb_heuristics=True, match_one=True, split_loops=True),
-        # Configuration(sim_mode=SimMode.SEMANTIC_SIM, sim_threshold=0.5, kb_heuristics=True, split_loops=True),
+    Configuration(sim_mode=SimMode.SEMANTIC_SIM, sim_threshold=0.7, kb_heuristics=True, match_one=True, split_loops=True),
+    Configuration(sim_mode=SimMode.SEMANTIC_SIM, sim_threshold=0.7, kb_heuristics=True, split_loops=True),
+    Configuration(sim_mode=SimMode.SEMANTIC_SIM, sim_threshold=0.5, kb_heuristics=True, match_one=True, split_loops=True),
+    Configuration(sim_mode=SimMode.SEMANTIC_SIM, sim_threshold=0.5, kb_heuristics=True, split_loops=True)
+
     ]
 
     results_file = "output/results_realworld" + time.strftime("%Y%m%d%H%M%S") + ".csv"
